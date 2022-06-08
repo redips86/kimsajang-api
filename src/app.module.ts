78a9e3a48269ad5febd669dbe27ab7dea5c66249
redips <redips@prismweaver.com>
2022-06-08 23:59:40 +0900

    [ksj] first commit

---
+++ src/app.module.ts
@@ -0,0 +1,20 @@
+import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { join } from 'path';
+import { ApolloServerPluginLandingPageLocalDefault } from 'apollo-server-core';
+import { UserModule } from './user/user.module';
+
+@Module({
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      playground: false,
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+      sortSchema: true,
+      plugins: [ApolloServerPluginLandingPageLocalDefault()],
+    }),
+    UserModule,
+  ],
+})
+export class AppModule {}
