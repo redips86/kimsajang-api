84f719cb8413cb7e7f1e3448b3d9db73a01c0c8f
redips <redips@prismweaver.com>
2022-06-09 22:54:12 +0900

    [ksj] prisma 연결

--- src/user/user.resolver.spec.ts
+++ src/user/user.resolver.spec.ts
@@ -1,12 +1,13 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UserResolver } from './user.resolver';
+import { UserService } from './user.service';
 
 describe('UserResolver', () => {
   let resolver: UserResolver;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [UserResolver],
+      providers: [UserResolver, UserService],
     }).compile();
 
     resolver = module.get<UserResolver>(UserResolver);
--- src/user/user.resolver.ts
+++ src/user/user.resolver.ts
@@ -1,9 +1,35 @@
-import { Query, Resolver } from '@nestjs/graphql';
+import { Args, Int, Mutation, Query, Resolver } from '@nestjs/graphql';
+import { UserService } from './user.service';
+import { User } from './entities/user.entity';
+import { CreateUserInput } from './dto/create-user.input';
+import { UpdateUserInput } from './dto/update-user.input';
 
-@Resolver()
+@Resolver(() => User)
 export class UserResolver {
-  @Query(() => Boolean)
-  isLogin(): boolean {
-    return true;
+  constructor(private readonly userService: UserService) {}
+
+  @Mutation(() => String)
+  createUser(@Args('createUserInput') createUserInput: CreateUserInput) {
+    return this.userService.create(createUserInput);
+  }
+
+  @Query(() => [User], { name: 'user' })
+  findAll() {
+    return this.userService.findAll();
+  }
+
+  @Query(() => String, { name: 'user' })
+  findOne(@Args('id', { type: () => Int }) id: number) {
+    return this.userService.findOne(id);
+  }
+
+  @Mutation(() => User)
+  updateUser(@Args('updateUserInput') updateUserInput: UpdateUserInput) {
+    return this.userService.update(updateUserInput.id, updateUserInput);
+  }
+
+  @Mutation(() => User)
+  removeUser(@Args('id', { type: () => Int }) id: number) {
+    return this.userService.remove(id);
   }
 }
