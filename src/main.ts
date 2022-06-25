d80c3d34032f403812cfd7e11967428239d4a776
redips <redips@prismweaver.com>
2022-06-25 10:31:17 +0900

    [ksj] port 3000 -> 4000
    Signed-off-by: redips <redips@prismweaver.com>

--- src/main.ts
+++ src/main.ts
@@ -16,7 +16,7 @@ async function bootstrap() {
     }*/),
   );
 
-  await app.listen(3000);
+  await app.listen(process.env.PORT || 3000);
   console.log(`Application is running on: ${await app.getUrl()}`);
 }
 
